134fa252a9d0264543c82d84e0ca336101d036fa
Mayannnkk <96884389+Mayannnkk@users.noreply.github.com>
2025-01-21 11:57:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,26 +21,27 @@ app.use(express.json());
 app.use(cors()); 
 // Routes
 // User Registration Route (POST)
-app.post('/api/v1/users/register', async (req, res) => {
-  const { username, email, password } = req.body;
+// app.post('/api/v1/users/register', async (req, res) => {
+//   // console.log(req.body)
+//   const { username, email, password } = req.body;
 
-  if (!username || !email || !password) {
-    return res.status(400).json({ message: 'All fields are required' });
-  }
+//   // if (!username || !email || !password) {
+//   //   return res.status(400).json({ message: 'All fields are required' });
+//   // }
 
-  try {
-    // Registration logic, e.g., hashing password and saving to database
-    // Example: 
-    // const hashedPassword = await bcrypt.hash(password, 10);
-    // const newUser = new User({ username, email, password: hashedPassword });
-    // await newUser.save();
+//   try {
+//     // Registration logic, e.g., hashing password and saving to database
+//     // Example: 
+//     // const hashedPassword = await bcrypt.hash(password, 10);
+//     // const newUser = new User({ username, email, password: hashedPassword });
+//     // await newUser.save();
     
-    res.status(201).json({ message: 'User registered successfully' });
-  } catch (error) {
-    console.error(error);  // This will help capture server-side errors
-    res.status(500).json({ message: 'Internal Server Error' });
-  }
-});
+//     res.status(201).json({ message: 'User registered successfully' });
+//   } catch (error) {
+//     console.error(error);  // This will help capture server-side errors
+//     res.status(500).json({ message: 'Internal Server Error' });
+//   }
+// });
 
 // User Routes
 app.use("/api/v1/users", require("./routes/userRoute"));
@@ -55,10 +56,14 @@ app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
+// app.post("/register",(req,res)=>{
+//   console.log(req.body)
+// })
 // Port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
